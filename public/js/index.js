d2de2ce2d5b2375671da814b05161ed71896fda4
Juliano Alves <jbalves0711@gmail.com>
2022-03-23 15:27:54 -0300

    Typo

--- public/js/index.js
+++ public/js/index.js
@@ -21,7 +21,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
 
     if (account) {
         if (account.password !== password) {
-            alert("Opps! Verifique o usuário ou a senha.");
+            alert("Ops! Verifique o usuário ou a senha.");
             return;
         }
 
